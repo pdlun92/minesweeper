60b7b1a05912731074a61610c3e397b0b9112046
Pat Lundgren <pdlun92@gmail.com>
2017-11-06 22:40:29 -0500

    completed third exercise

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,17 +1,42 @@
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for (i = 0; i < numberOfRows; i++) {
+    let row = [];
+    for (j = 0; j < numberOfColumns; j++) {
+      row.push(' ');
+    };
+  board.push(row);
+  };
+  return board;
+}
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (i = 0; i < numberOfRows; i++) {
+    let row = [];
+    for (j = 0; j < numberOfColumns; j++) {
+      row.push(null);
+    };
+  board.push(row);
+  };
+  let numberOfBombsPlaced = 0;
+  // TODO add code that ensures bombs will not be placed on top of each other
+  while (numberOfBombsPlaced < numberOfBombs) {
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+  }
+  return board;
+}
+
 const printBoard = (board) => {
-  console.log('Current Board:');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+}
 
-};
-const board = [
- [' ', ' ', ' '],
- [' ', ' ', ' '],
- [' ', ' ', ' ']
-];
-
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+console.log('Player Board:');
+printBoard(playerBoard);
+console.log('Bomb Board:');
+printBoard(bombBoard);
